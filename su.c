7b68818b1e2d21ced71fb3b7ee22a4d05a5434dc
suvithamohanapriya <swarnaprabhamaha@gmail.com>
2017-06-30 11:16:58 +0530

    Create su.c

---
+++ su.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+clrscr();
+printf("\n enter teh value");
+scanf("%d",&a);
+if(a>0)
+    printf("\n word is possitive");
+      else if(a<0)
+         printf('\n word is negative");
+           else
+             printf("\nzero");
+ return 0;
+ }
